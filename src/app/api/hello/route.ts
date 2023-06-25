b234541a7e62527a7453aaf4187cbb34d45bf5a5
finnelliott <finn@finnelliott.com>
2023-06-25 14:43:08 +0100

    --

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -54,7 +54,77 @@ async function checkSuitability(item: string) {
       "messages": [
         {
           "role": "system",
-          "content": "The user has the following skill profile:\n- Next.js\n- Webflow\n- Expert REDCap developer\n- Full-stack web development\n- Web design\n\nEvaluate the user's suitability for the job description they provide. If they are suitable, send them a suggested proposal. If they are not suitable, send them an email with the reasoning why they are not suitable."
+          "content": `The user has the following profile:
+---
+**Educational Background:**
+
+- Pate's Grammar School: 3 As in Maths, Economics, and Psychology
+- University of Bath: BSc in Business Administration
+
+**Work Experience:**
+
+1. Vita Coco, London (Innovation Intern)
+   - Worked with design and marketing teams to apply new branding to over 30 SKUs
+   - Conducted tasks related to bringing a new product to market, including sourcing suppliers, taste testing, shelf life testing, and cost forecasting
+   - Presented work by external lawyers regarding packaging claims to the head of marketing and managing director
+
+2. Capita Scaling Partner (Origination Analyst)
+   - Outreach and initial meetings with startup founders
+   - Organisation and marketing of online ‘Dragon’s Den-style’ pitching events
+   - Financial modelling of investment opportunities and presenting findings to partners
+
+3. Freelance Web Designer & Developer
+   - Web Development (Next.js, MongoDB, Prisma, TailwindCSS, Vercel)
+   - Design (Figma) & SEO (SEMRush)
+   - Experience with common web builders (Webflow, Squarespace, Wix)
+   - Third-party integrations (Stripe, Mailchimp, Resend, Hubspot, Clerk, OpenAI, Stability AI, Google APIs, Payload CMS, Prismic)
+
+**Project Management:**
+
+- Familiar with Notion, Trello, and Monday
+- Primarily works through email
+
+**Marketing:**
+
+- Platforms: Google Ads, Facebook Ads, SEMRush, Instagram, Facebook, Twitter
+- Adobe Suite: Photoshop, Illustrator, Premier Pro
+- Full marketing service for clients, including ad strategy, creation and optimisation, content creation, and social media management
+
+**Sales & Business Development:**
+
+- Founder of a profitable inter-city travel and events company for students
+- Identified market gap, sold-out events, negotiated with event organisers and travel companies, and built a custom e-ticketing solution
+
+**Graphic Design & Multimedia Production:**
+
+- Heavy use of Figma as a web designer
+- Experience with Illustrator
+
+**Writing & Copywriting:**
+
+- Content creation for landing pages of businesses as a web designer
+- Focus on clear copy that highlights benefits rather than features
+
+**Research:**
+
+- Conducted an extensive research project in the final year of University, interviewing members of Boeing Defence UK's team regarding social value criteria in the UK's public sector contracting evaluation system
+
+**Finance:**
+
+- Financial modelling at Capita Scaling Partner, using Excel to create sophisticated models based on existing financial data from prospective partner startups and presenting findings to partners
+
+**Industry-specific Software:**
+
+- Hubspot, Monday
+
+**Career Goals:**
+
+- Seeking contracts with small businesses to improve their online presence
+- Creating new websites or web applications using Next.js, including content-based solutions with a CMS, SaaS solutions, or eCommerce solutions
+- Open to contracts outside of this realm if the required skill exists within abilities
+
+---
+Evaluate the user's suitability for the job description they provide. If they are suitable, send them a suggested proposal via email. If they are not suitable, send them an email with the reasoning why they are not suitable.`
         },
         {
           "role": "user",
